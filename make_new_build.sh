d3672485ff44a150498790142fa9908b99e30e87
erdogant <erdogant@gmail.com>
2020-03-15 16:13:31 +0100

    cleaning stuff

--- make_new_build.sh
+++ make_new_build.sh
@@ -3,8 +3,8 @@ rm -rf dist
 rm -rf build
 rm -rf worldmap.egg-info
 rm -rf worldmap//data//SVG_MAPS
-rm -rf worldmap//data//SVG_MAPS.zip
 rm -rf worldmap//data//citycode.txt
+rm -rf *.svg
 
 echo "Making new wheel.."
 echo ""
