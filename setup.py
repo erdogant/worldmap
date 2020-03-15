ce78545d535f3ab212e18b87ad3e942116f6097c
erdogant <erdogant@gmail.com>
2020-03-15 14:33:39 +0100

    setup file update

--- setup.py
+++ setup.py
@@ -1,19 +1,25 @@
 import setuptools
-import versioneer
-new_version='0.1.4'
+import re
 
+# versioning ------------
+VERSIONFILE="worldmap/__init__.py"
+getversion = re.search( r"^__version__ = ['\"]([^'\"]*)['\"]", open(VERSIONFILE, "rt").read(), re.M)
+if getversion:
+    new_version = getversion.group(1)
+else:
+    raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE,))
+
+# Setup ------------
 with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
-     install_requires=['numpy','pandas','tqdm','seaborn','sklearn','svgpathtools'],
+     install_requires=['wget','matplotlib','numpy','pandas','svgpathtools','colourmap','tqdm','fuzzywuzzy[speedup]','sklearn'],
      python_requires='>=3',
      name='worldmap',
      version=new_version,
-#     version=versioneer.get_version(),    # VERSION CONTROL
-#     cmdclass=versioneer.get_cmdclass(),  # VERSION CONTROL
      author="Erdogan Taskesen",
      author_email="erdogant@gmail.com",
-     description="Color the worldmap",
+     description="Python package worldmap",
      long_description=long_description,
      long_description_content_type="text/markdown",
      url="https://github.com/erdogant/worldmap",
@@ -22,7 +28,7 @@ setuptools.setup(
      include_package_data=True, # Must be true to include files depicted in MANIFEST.in
      classifiers=[
          "Programming Language :: Python :: 3",
-         "License :: OSI Approved :: Apache Software License",
+         "License :: OSI Approved :: MIT License",
          "Operating System :: OS Independent",
      ],
  )
