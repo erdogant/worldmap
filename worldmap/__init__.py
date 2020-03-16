58dc22df618f71738b748897d81862eaa5a36d28
erdogant <erdogant@gmail.com>
2020-03-16 21:00:32 +0100

    version update

--- worldmap/__init__.py
+++ worldmap/__init__.py
@@ -8,7 +8,7 @@ from worldmap.worldmap import (
 
 __author__ = 'Erdogan Tasksen'
 __email__ = 'erdogant@gmail.com'
-__version__ = '0.1.0'
+__version__ = '0.1.5'
 
 # module level doc-string
 __doc__ = """
